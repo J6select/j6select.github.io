d8fb95ae382e36d7a529a232dbea2355ab953ec0
J6select <40902585+J6select@users.noreply.github.com>
2018-07-22 19:33:20 -0700

    Create 05.js

---
+++ module/05.js
@@ -0,0 +1,12 @@
+var table = prompt("Type in number to multiply", 1);             // Unit of table
+var operator = 'multiplication'; // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>';              // Message
+
+while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+}
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
